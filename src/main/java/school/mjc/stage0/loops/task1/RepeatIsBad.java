546f6f5826aac1395700fba92fb7ee26cedb18eb
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-30 21:07:10 +0400

    update

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,9 +1,18 @@
 package school.mjc.stage0.loops.task1;
 
 public class RepeatIsBad {
+    private static final int MAX_COUNT = 20;
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
+        printMessage(MAX_COUNT);
+    }
 
+    private void printMessage(int count) {
+        if (count > 0) {
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+            printMessage(count - 1);
+        }
     }
 }
+
