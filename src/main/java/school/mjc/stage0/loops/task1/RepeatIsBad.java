2909acd6a4693c89874dd90b8f16c2811e8442ad
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-31 09:26:19 +0400

    update

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,18 +1,34 @@
 package school.mjc.stage0.loops.task1;
 
-public class RepeatIsBad {
-    private static final int MAX_COUNT = 0;
+import java.util.Observable;
+import java.util.Observer;
+
+class RepeatIsBad extends Observable {
+    private static final int MAX_COUNT = 20;
+    private int remainingCount = MAX_COUNT;
 
     @SuppressWarnings("java:S106")
-    public void repeatIsBad(){
-        printMessage(MAX_COUNT);
+    public void repeatIsBad() {
+        addObserver(new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+                if (remainingCount > 0) {
+                    System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+                    remainingCount--;
+                    setChanged();
+                    notifyObservers();
+                }
+            }
+        });
+
+        setChanged();
+        notifyObservers();
     }
 
-    private void printMessage(int count) {
-        if (count > 0) {
-            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
-            printMessage(count + 1);
-        }
+    public static void main(String[] args) {
+        RepeatIsBad repeater = new RepeatIsBad();
+        repeater.repeatIsBad();
     }
 }
+
 
